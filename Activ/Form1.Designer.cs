19bdf091119dd6c963d484755c91a68d94d4502c
Gonzalo <gonzalopasos87@gmail.com>
2023-09-05 13:15:10 -0300

    Arme la pagina principal con tres buttons Listar, Agregar, Buscar

---
+++ Activ/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace Activ
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btListar = new System.Windows.Forms.Button();
+            this.btAgregar = new System.Windows.Forms.Button();
+            this.btBuscar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btListar
+            // 
+            this.btListar.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btListar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btListar.Location = new System.Drawing.Point(144, 178);
+            this.btListar.Name = "btListar";
+            this.btListar.Size = new System.Drawing.Size(86, 40);
+            this.btListar.TabIndex = 0;
+            this.btListar.Text = "Listado";
+            this.btListar.UseVisualStyleBackColor = true;
+            this.btListar.Click += new System.EventHandler(this.btListar_Click);
+            // 
+            // btAgregar
+            // 
+            this.btAgregar.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btAgregar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btAgregar.Location = new System.Drawing.Point(526, 178);
+            this.btAgregar.Name = "btAgregar";
+            this.btAgregar.Size = new System.Drawing.Size(85, 40);
+            this.btAgregar.TabIndex = 2;
+            this.btAgregar.Text = "Agregar";
+            this.btAgregar.UseVisualStyleBackColor = true;
+            this.btAgregar.Click += new System.EventHandler(this.btAgregar_Click);
+            // 
+            // btBuscar
+            // 
+            this.btBuscar.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btBuscar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btBuscar.Location = new System.Drawing.Point(336, 178);
+            this.btBuscar.Name = "btBuscar";
+            this.btBuscar.Size = new System.Drawing.Size(86, 40);
+            this.btBuscar.TabIndex = 3;
+            this.btBuscar.Text = "Buscar";
+            this.btBuscar.UseVisualStyleBackColor = true;
+            this.btBuscar.Click += new System.EventHandler(this.btBuscar_Click);
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btBuscar);
+            this.Controls.Add(this.btAgregar);
+            this.Controls.Add(this.btListar);
+            this.Name = "frmPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Principal";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btListar;
+        private System.Windows.Forms.Button btAgregar;
+        private System.Windows.Forms.Button btBuscar;
+    }
+}
+
---
+++ Activ/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Activ
+{
+    public partial class frmPrincipal : Form
+    {
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btListar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btAgregar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btBuscar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
