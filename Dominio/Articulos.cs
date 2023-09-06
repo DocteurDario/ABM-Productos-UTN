37905b7c89d1eee7234d83331a56c3afe5d518e4
nahuelRomero1 <naahuel96@hotmail.com>
2023-09-05 23:13:33 -0300

    cree clase articulo, marca, categoria y la ordene en una libreria llamada dominio

---
+++ Dominio/Articulos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+   public class Articulos
+    {
+        public int nroArticulo { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public Marca Marcas { get; set; }
+        public Categorias Categoria { get; set; }
+        public decimal Precio { get; set; }
+    }
+}
---
+++ Dominio/Categorias.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+   public  class Categorias
+    {
+        public Categorias()
+        {
+
+        }
+        public Categorias(int Id, string Desc)
+        {
+            Descripcion = Desc;
+            this.Id = Id;
+        }
+        public int Id { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public override string ToString()
+        {
+            return Descripcion;
+        }
+    }
+}
