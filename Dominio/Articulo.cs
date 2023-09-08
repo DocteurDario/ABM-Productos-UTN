06b18cb558eb23dc5f7bbbe46f8c406bbaf03323
DocteurDario <docteurdario@gmail.com>
2023-09-07 22:06:48 -0300

    se arreglo nombre de las clases

--- Dominio/Articulo.cs
+++ Dominio/Articulo.cs
@@ -6,13 +6,13 @@ using System.Threading.Tasks;
 
 namespace Dominio
 {
-   public class Articulos
+   public class Articulo
     {
         public int nroArticulo { get; set; }
         public string Nombre { get; set; }
         public string Descripcion { get; set; }
-        public Marcas Marcas { get; set; }
-        public Categorias Categoria { get; set; }
+        public Marca Marca { get; set; }
+        public Categoria Categoria { get; set; }
         public decimal Precio { get; set; }
     }
 }
